028fa49b6ff2d2cc6161075c28ccd361a15aedf5
MyeclipseEL <1228537086@qq.com>
2020-10-16 12:28:22 +0800

    deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 APP_NAME=testpip-0.0.1.jar
-APP_DIR=target/
+APP_DIR=/var/jenkins_home/workspace/demo/target/
 usage(){
     echo "Usage sh执行脚本.sh [start|stop|restart|status]"
     exit 1
