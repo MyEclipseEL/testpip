27ee90131fa5818a31ffa6fd75b085aaf0cbc58b
ShenJinpeng <s1228537086@qq.com>
2020-10-16 09:13:39 +0800

    Initial commit

---
+++ src/main/java/com/test/testpip/TestpipApplication.java
@@ -0,0 +1,13 @@
+package com.test.testpip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestpipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestpipApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/test/testpip/TestpipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.testpip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestpipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
